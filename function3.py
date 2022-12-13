135fa53e44a4482e252e0d9c51e374f27497318c
jeongyeol-python <pass10@nate.com>
2022-12-13 16:07:06 +0900

    함수연습6

--- function3.py
+++ function3.py
@@ -47,3 +47,36 @@ def connectURI(server, port):
 
 print( connectURI("credu.com", "80") )
 print( connectURI(port="8080", server="credu.com") ) 
+
+#가변적인 경우(가변인자 *)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+#호출
+print( union("HAM", "EGG") )
+print( union("HAM", "EGG", "SPAM") )
+
+#정의되지않은 인자(필수와 옵션이 같이 있는 경우 **)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+
+#call
+print( userURIBuilder("credu.com","80",id="kim", passwd="1234") )
+print( userURIBuilder("credu.com","80",id="kim", passwd="1234", age="30") )
+
+#간단, 함수정의 문법 > 람다함수
+g = lambda x,y:x*y
+print( g(3,4) )
+print( g(5,6) )
+
+#일회성 람다함수
+print( (lambda x:x*x)(3) )
+print( globals() )
